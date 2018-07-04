e14a425606b2393fd0c8f02315f5df38eaecf352
Mark Boon <boomer_cant_be_stuffed@Hotmail.com>
2018-07-04 22:09:10 +0800

    added smooth scrolling

--- src/home/page.js
+++ src/home/page.js
@@ -18,20 +18,22 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div className='HomePage'>
+
         <div className='header'>
           <Avatar
             alt='MDB'
             className='avatar'
-            src={require('../static/selfie3.jpg')}
+            src={require('../static/jpg/selfie4.jpg')}
           />
         </div>
-        <div className='aboutme'>
+
+        <div className='about-me'>
           <Typography
             classes={{
-              root: 'aboutme-wrapper'
+              root: 'aboutme-text'
             }}
           >
-            I am a Full Stack Web Developer and UI/UX designer based in Perth, Australia.<br/>
+            I am a Full Stack Web Developer and UI/UX Designer based in Perth, Australia.<br />
             I have a passion for creating intuitive and responsive user experiences.
           </Typography>
         </div>
